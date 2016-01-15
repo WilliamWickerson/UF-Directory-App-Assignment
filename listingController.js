6d4ccb11e0c918d403ca3335d87b0520c8c71f5f
willie7296@yahoo.com <willie7296@yahoo.com>
2016-01-15 01:24:00 -0500

    yolo

--- listingController.js
+++ listingController.js
@@ -2,13 +2,27 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
   function($scope, Listings) {
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
-
     /* 
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.search = function() {
+      $scope.searchQuery = angular.copy($scope.query);
+    };
+    $scope.enter = function() {
+      if (keyEvent.which === 13)
+        $scope.search();
+    }
+    $scope.addListing = function() {
+      $scope.add.code = angular.uppercase($scope.add.code);
+      $scope.listings.push($scope.add);
+      $scope.add = null;
+    };
+    $scope.deleteListing = function(index) {
+      $scope.listings.splice($scope.listings.indexOf(index),1);  
+    };
+    $scope.showDetails = function(index) {
+      $scope.detailedInfo = index;
+    };
   }
 ]);
