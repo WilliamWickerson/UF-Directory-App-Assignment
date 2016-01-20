be28645c49365bcbcc0f478839110237e060941e
WilliamWickerson <willie7296@gmail.com>
2016-01-19 22:11:27 -0500

    I did it

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -1,23 +1,43 @@
 'use strict';
-/* 
-  Import modules/files you may need to correctly run the script. 
-  Make sure to save your DB's uri in the config file, then import it with a require statement!
- */
+
 var fs = require('fs'),
-    mongoose = require('mongoose'), 
-    Schema = mongoose.Schema, 
-    Listing = require('./ListingSchema.js'), 
-    config = require('./config');
+    mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    Listing = require('./ListingSchema.js'),
+    config = require('./config'),  
+    listings = require('./listings.json');
 
 /* Connect to your database */
+
+mongoose.connect(config.db.uri, function(err) {
+  if (err) throw err;
+});
 
 /* 
   Instantiate a mongoose model for each listing object in the JSON file, 
   and then save it to your Mongo database 
  */
 
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+
+var length = listings.entries.length;
+for (var i = 0; i < length; i++) {
+  var temp = new Listing({
+    code: listings.entries[i].code,
+    name: listings.entries[i].name,
+    coordinates: listings.entries[i].coordinates,
+    address: listings.entries[i].address
+  });
+  temp.save(function(err) {
+    if (err) throw err;
+  });
+};
+
 
 /* 
   Once you've written + run the script, check out your MongoLab database to ensure that 
   it saved everything correctly. 
  */
+});
---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  db: {
+    uri: 'mongodb://test:test@ds047335.mongolab.com:47335/wickerson', //place the URI of your mongo database here.
+  }
+};
